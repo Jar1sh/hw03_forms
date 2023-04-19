6df7d4c9786612f71431c16067244e0b747d2f54
Ярослав Кисленков <Jarrpen@yandex.ru>
2023-04-19 13:19:42 +0300

    First Blood

---
+++ yatube/posts/admin.py
@@ -0,0 +1,26 @@
+from django.contrib import admin
+
+from .models import Group, Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    def __str__(self):
+        return self.text
+    list_display = (
+        'pk',
+        'text',
+        'pub_date',
+        'author',
+        'group',
+    )
+    list_editable = ('group',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+    date_hierarchy = 'pub_date'
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
---
+++ yatube/posts/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
+
+
+class Group(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Название группы',
+        help_text='Введите название группы',
+    )
+    slug = models.SlugField(
+        unique=True,
+        verbose_name='Адрес для страницы группы',
+        help_text='Адрес заполняется автоматически',
+    )
+    description = models.TextField(
+        verbose_name='Описание группы',
+        help_text='Введите описание группы',
+    )
+
+    class Meta:
+        verbose_name = 'Группа'
+        verbose_name_plural = 'Группы'
+
+    def __str__(self):
+        return self.title
+
+
+class Post(models.Model):
+    text = models.TextField(
+        verbose_name='Текст сообщения',
+        help_text='Введите текст сообщения',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Дата публикации',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="posts",
+        verbose_name='Автор',
+        help_text='Выберите автора',
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.SET_NULL,
+        related_name='posts',
+        blank=True,
+        null=True,
+        verbose_name='Группа',
+        help_text='Выберите группу',
+    )
+
+    class Meta:
+        ordering = ['-pub_date']
+        verbose_name = 'Сообщение'
+        verbose_name_plural = 'Сообщения'
+
+    def __str__(self):
+        return self.text
