36d37b1908d842bd49218b2448b552dc241ae2d2
Ярослав Кисленков <Jarrpen@yandex.ru>
2023-04-23 12:18:21 +0300

    Correct ORM

--- yatube/posts/utils.py
+++ yatube/posts/utils.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.paginator import Paginator
 
+
 def paginate_posts(request, posts):
     paginator = Paginator(posts, settings.POSTS_PER_PAGE)
     page_number = request.GET.get('page')
